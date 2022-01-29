bf474a6ef8fbd772679ca3e22e43a5845213c02a
Alejandro Alomia <alejandroalomia730@gmail.com>
2022-01-29 13:35:10 -0500

    C is <text>

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -21,7 +21,7 @@ def hbnb():
 @app.route('/c/<text>')
 def c_is(text):
         """display “C ” followed by the value of the text variable"""
-        return 'C is {}'.format(text)
+        return 'C {}'.format(text)
 
 if __name__ == '__main__':
         app.run(host='0.0.0.0', port=5000)
